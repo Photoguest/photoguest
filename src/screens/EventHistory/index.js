276d1a376eeea9d2f55e96534ea9cff5b66f7e59
Vinicius <pauloviniciusrp9@gmail.com>
2021-01-24 23:29:43 -0300

    [INIT] Event Histoty

---
+++ src/screens/EventHistory/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LinearGradient from 'react-native-linear-gradient';
+import AsyncStorage from '@react-native-community/async-storage';
+
+import { StatusBar, FlatList, TouchableOpacity } from 'react-native';
+
+const Item = ({ text }) => (
+  
+)
+
+
+
